9d5e3ef40daec42d5158a4f0e52b80a84463fd21
Antonie Prigoreanu <antonieprigoreanu2004@gmail.com>
2025-03-23 21:20:11 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Inițializare tablou de liste pentru fiecare literă (a-z)
+        List<string>[] cuvinte = new List<string>[26];
+        for (int i = 0; i < 26; i++)
+        {
+            cuvinte[i] = new List<string>();
+        }
+
+        try
+        {
+            // Citire cuvinte din fișier
+            string[] linii = File.ReadAllLines("cuvinte.txt");
+
+            // Procesare fiecare cuvânt
+            foreach (string linie in linii)
+            {
+                string[] cuvinteLinie = linie.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string cuvant in cuvinteLinie)
+                {
+                    if (!string.IsNullOrEmpty(cuvant))
+                    {
+                        char primaLitera = char.ToLower(cuvant[0]);
+                        if (primaLitera >= 'a' && primaLitera <= 'z')
+                        {
+                            int index = primaLitera - 'a';
+                            cuvinte[index].Add(cuvant);
+                        }
+                    }
+                }
+            }
+
+            // Afișare tablou
+            for (int i = 0; i < 26; i++)
+            {
+                char litera = (char)('a' + i);
+                Console.WriteLine($"Cuvinte care incep cu '{litera}' sau '{char.ToUpper(litera)}':");
+                if (cuvinte[i].Count == 0)
+                {
+                    Console.WriteLine(" - Niciun cuvant\n");
+                }
+                else
+                {
+                    foreach (string cuvant in cuvinte[i])
+                    {
+                        Console.WriteLine($" - {cuvant}");
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Eroare: {ex.Message}");
+        }
+    }
+}
